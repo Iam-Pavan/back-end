4c027fd350a468740469faec039bda73978a9d88
Iam-Pavan <pavanachar36@gmail.com>
2024-06-23 10:52:05 +0530

    python

---
+++ python/app.py
@@ -0,0 +1,26 @@
+# python
+# print function
+print("python",100)
+
+name = 'pavan'
+age = 20
+print(name,' is ',age)
+print("pavan is from turkey")
+print(name)
+
+# strings in python
+print('strings in python **********')
+print('hi \n how are you')
+# lower() and upper function in string
+# lower case and upper case function
+# islower() and isupper() can check the strings in upper or lower
+print(name.lower().islower())
+
+# lenght function
+print("len(name) number: ",len(name))
+# index function letter
+print("name.[0] letter: ",name[0])
+# index number
+print("index('p') number: ",name.index('p'))
+
+
